771196b1cfb3950878b0ca11a22431b617250f64
BrunahAlves <Brunahalves94@gmail.com>
2017-06-11 21:13:48 -0300

    Cria cria-etiqueta e edita EtiquetaServlet para criar etiqueta(6)

--- src/java/br/cesjf/lppo/servlet/EtiquetaServlet.java
+++ src/java/br/cesjf/lppo/servlet/EtiquetaServlet.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
@@ -33,14 +35,14 @@ public class EtiquetaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                if (request.getServletPath().contains("/editarEtiqueta.html")) {
+        if (request.getServletPath().contains("/editarEtiqueta.html")) {
             
         } else if(request.getServletPath().contains("/excluirEtiqueta.html")){
             
         } else if(request.getServletPath().contains("/listarEtiqueta.html")){
             doListarGet(request, response);
         } else if(request.getServletPath().contains("/criarEtiqueta.html")){
-            
+            doCriarGet(request, response);
         }
  
     }
@@ -48,7 +50,11 @@ public class EtiquetaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+          if (request.getServletPath().contains("/editarEtiqueta.html")) {
+           
+        } if (request.getServletPath().contains("/criarEtiqueta.html")) {
+            doCriarPost(request, response);
+        } 
     }
 
     private void doListarGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -58,6 +64,25 @@ public class EtiquetaServlet extends HttpServlet {
         
         request.setAttribute("etiquetas", etiquetas);
         request.getRequestDispatcher("WEB-INF/listar-etiquetas.jsp").forward(request, response);
+    }
+
+    private void doCriarGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.getRequestDispatcher("WEB-INF/cria-etiqueta.jsp").forward(request, response);
+    }
+
+    private void doCriarPost(HttpServletRequest request, HttpServletResponse response) {
+        Etiqueta etiqueta1 = new Etiqueta();
+        etiqueta1.setReferencia_autor(request.getParameter("referencia_autor"));
+        etiqueta1.setReferencia_tarefa(request.getParameter("referencia_tarefa"));
+        etiqueta1.setTitulo(request.getParameter("titulo"));
+
+        EtiquetaJpaController dao = new EtiquetaJpaController(ut, emf);
+        try {
+            dao.create(etiqueta1);
+            response.sendRedirect("listarEtiqueta.html");
+        } catch (Exception ex) {
+            Logger.getLogger(UsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 
---
+++ web/WEB-INF/cria-etiqueta.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : cria-etiqueta
+    Created on : 11/06/2017, 21:07:41
+    Author     : Bruna Alves
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Cria Etiqueta</title>
+    </head>
+    <body>
+        <h1>Cria etiqueta!</h1>
+        <form method="post">
+            <label>Referencia para um autor: <input name="referencia_autor" /></label>
+            <label>Referência para uma tarefa: <input name="referencia_tarefa" /></label>
+            <label>Titulo: <input name="titulo" /></label>
+            <input type="submit" />
+    </body>
+</html>
